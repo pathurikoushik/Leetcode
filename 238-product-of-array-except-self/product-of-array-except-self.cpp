db717784b562cee623aa7beb305193c16f94e077
Koushik Pathuri <119720431+pathurikoushik@users.noreply.github.com>
2025-03-10 22:02:49 +0530

    Time: 2 ms (44.70%) | Memory: 40.2 MB (79.70%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -2,18 +2,20 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n = nums.size();
-        vector<int> answer(n, 1);
+        vector<int> ans(n, 1);
+
         int prefix = 1;
-        for (int i = 0; i < n; i++) {
-            answer[i] = prefix;
+        for (int i = 0; i < n; ++i) {
+            ans[i] = prefix;
             prefix *= nums[i];
         }
+
         int suffix = 1;
-        for (int i = n - 1; i >= 0; i--) {
-            answer[i] *= suffix;
+        for (int i = n - 1; i >= 0; --i) {
+            ans[i] *= suffix;
             suffix *= nums[i];
         }
-        
-        return answer;
+
+        return ans;
     }
 };
