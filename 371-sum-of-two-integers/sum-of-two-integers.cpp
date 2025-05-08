8e29ba762a6372b6ed26895b45d051bacbd905ad
Koushik Pathuri <119720431+pathurikoushik@users.noreply.github.com>
2025-05-08 08:29:56 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (2.40%) - LeetSync

---
+++ 371-sum-of-two-integers/sum-of-two-integers.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int getSum(int a, int b) {
+        while(b != 0) {
+            unsigned carry = (unsigned)(a & b) << 1;
+            a = a ^ b;
+            b = carry;
+        }
+        return a;
+    }
+};
