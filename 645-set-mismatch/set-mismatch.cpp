7670883761531419083af64509378632fc70ca7e
Koushik Pathuri <119720431+pathurikoushik@users.noreply.github.com>
2025-05-08 08:03:39 +0530

    Time: 3 ms (53.16%) | Memory: 26.3 MB (45.34%) - LeetSync

--- 645-set-mismatch/set-mismatch.cpp
+++ 645-set-mismatch/set-mismatch.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
         int n = nums.size();
-        vector<int> count(n+ 1, 0);
-
+        vector<int> count(n + 1, 0);
+        
         for(int num : nums) {
             count[num]++;
         }
         int duplicate = -1, missing = -1;
-        for (int i = 1; i <= n;++i) {
+        for (int i = 1; i <= n;++i){
             if(count[i] == 2) {
                 duplicate = i;
             }
