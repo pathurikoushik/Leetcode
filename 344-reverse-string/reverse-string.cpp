f89724b3280eec66fd504ea73ad1669666b74a98
Koushik Pathuri <119720431+pathurikoushik@users.noreply.github.com>
2025-05-15 09:53:51 +0530

    Time: 0 ms (100.00%) | Memory: 27.1 MB (80.22%) - LeetSync

--- 344-reverse-string/reverse-string.cpp
+++ 344-reverse-string/reverse-string.cpp
@@ -1,10 +1,6 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        int l = 0;
-        int r = s.size() - 1;
-        while(l < r){
-            swap(s[l++],s[r--]);
-        }
+        reverse(s.begin(), s.end());
     }
 };
