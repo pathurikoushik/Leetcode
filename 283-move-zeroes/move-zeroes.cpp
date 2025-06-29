6a81aa7555eaceb22acb477d5e576b4ed66be6e3
Koushik Pathuri <119720431+pathurikoushik@users.noreply.github.com>
2025-06-29 20:40:35 +0530

    Time: 0 ms (100.00%) | Memory: 24 MB (17.94%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -4,6 +4,7 @@ public:
         int nonZeroIndex = 0;
         for(int i = 0; i < nums.size(); i++) {
             if(nums[i] != 0) {
+
                 swap(nums[nonZeroIndex], nums[i]);
                 nonZeroIndex++;
             }
