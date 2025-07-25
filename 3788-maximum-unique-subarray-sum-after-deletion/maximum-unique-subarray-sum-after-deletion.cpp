a68b43ff6dc6795e37b615044a470948295c0551
Koushik Pathuri <119720431+pathurikoushik@users.noreply.github.com>
2025-07-25 08:23:29 +0530

    Time: 0 ms (100.00%) | Memory: 29.6 MB (57.77%) - LeetSync

--- 3788-maximum-unique-subarray-sum-after-deletion/maximum-unique-subarray-sum-after-deletion.cpp
+++ 3788-maximum-unique-subarray-sum-after-deletion/maximum-unique-subarray-sum-after-deletion.cpp
@@ -1,15 +1,29 @@
+// class Solution {
+// public:
+//     int maxSum(vector<int>& nums) {
+//         int maxCurr = nums[0];
+//         int maxGlob = nums[0];
+//         for (int i = 1; i < nums.size(); i++) {
+//             maxCurr = max(nums[i], maxCurr + nums[i]);
+//             if(maxCurr > maxGlob)
+//                 maxGlob = maxCurr;
+//         }
+//         return maxGlob;
+//     }
+// };
+
 class Solution {
 public:
     int maxSum(vector<int>& nums) {
-        int maxElement = *max_element(nums.begin(), nums.end());
-        if (maxElement <= 0) {
-            return maxElement;
+        int maxEle = *max_element(nums.begin(), nums.end());
+        if(maxEle <= 0) {
+            return maxEle;
         }
-        
-        unordered_set<int> uniqueNums(nums.begin(), nums.end());
+
+        unordered_set<int > uniquenames(nums.begin(), nums.end());
         int sum = 0;
-        for (int num : uniqueNums) {
-            if (num > 0) {
+        for(int num : uniquenames) {
+            if(num > 0) {
                 sum += num;
             }
         }
